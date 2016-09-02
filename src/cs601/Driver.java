a47dee946a1b11cd5cd274a2990569e3f7bd908b
Omer Akin <akin__08@hotmail.com>
2016-09-02 21:38:29 +0300

    new add three classes

--- src/cs601/Driver.java
+++ src/cs601/Driver.java
@@ -1,10 +1,13 @@
 package cs601;
 
+import cs601.hotelapp.Address;
+import cs601.hotelapp.Hotel;
 import cs601.hotelapp.HotelData;
 import java.nio.file.Paths;
 
 public class Driver {
 	public static void main(String[] args) {
+		
 		HotelData data = new HotelData();
 		
 		// Load hotel info from hotels200.json
@@ -15,5 +18,14 @@ public class Driver {
 		data.loadReviews(Paths.get("input/reviews")); 
 		data.printToFile(Paths.get("outputFile"));
 		
+		/*
+		Address a1 = new Address("a", "b", "c", 1, 1);
+		Address a2 = new Address("aa", "bb", "cc", 1, 1);
+		
+		Hotel h1 = new Hotel("11111", "omer", a1);
+		Hotel h2 = new Hotel("22222", "omer", a2); 
+		
+		System.out.println(h1.compareTo(h2));
+		*/
 	}
 }
