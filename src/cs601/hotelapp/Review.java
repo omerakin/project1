dd612267d42161c300df656db7486bcd64938cb9
Omer Akin <akin__08@hotmail.com>
2016-09-02 23:45:49 +0300

    new things added.

--- src/cs601/hotelapp/Review.java
+++ src/cs601/hotelapp/Review.java
@@ -43,13 +43,12 @@ public class Review implements Comparable<Review>{
 			
 			if(date1.compareTo(date2)>0){
 				System.out.println("date1 is after date2");
+				return date1.compareTo(date2); 
 			} else if (date1.compareTo(date2)<0) {
 				System.out.println("date1 is before date1");
+				return date1.compareTo(date2);
 			} else if (date1.compareTo(date2)==0) {
 				System.out.println("date1 is equal to date2");
-				/*
-				 * 
-				 */
 				return username.compareTo(o.getUsername());
 			} else {
 				System.out.println("Something is wrong with date!!!");
